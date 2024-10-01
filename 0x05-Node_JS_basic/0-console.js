9b53f2f8b68f25520c85e8ab81f3e3c499cb94de
Ahadi <ahadic044@gmail.com>
2024-10-01 18:30:25 +0200

    using lint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,3 @@
-#!/usr/bin//env node
-// displays message on the standard output using process api
-
-
-function displayMessage(text){
-    process.stdout.write(text + '\n');
-}
-
+const displayMessage = (message) => console.log(message);
 
 module.exports = displayMessage;
