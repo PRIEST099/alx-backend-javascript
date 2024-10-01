0841b20c60daac9a67af0d29697ef11e5af69d45
Ahadi <ahadic044@gmail.com>
2024-10-01 18:35:13 +0200

    adding description

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays message to STDOUT
+ * @param {String} d message to display
+ * @returns nothing
+ */
 const displayMessage = (d) => console.log(d);
 
 module.exports = displayMessage;
